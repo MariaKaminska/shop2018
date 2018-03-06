42786fae624625f9f76763159670727e1f23d73d
Maria Kamińska <>
2018-03-06 13:47:35 +0100

    zmiana na mapę

--- shop2018/src/shop2018/Shop2018.java
+++ shop2018/src/shop2018/Shop2018.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package shop2018;
 
-/**
- *
- * @author Student
- */
+import java.math.BigDecimal;
+
 public class Shop2018 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Produkt p1=new Produkt("telewizor",new BigDecimal("2500.99"));
+        Produkt p2=new Produkt("szafa",new BigDecimal("300.50"));
+        Produkt p3=new Produkt("obraz",new BigDecimal("200.39"));
+        Produkt p4=new Produkt("komoda",new BigDecimal("200.99"));
+        Produkt p5=new Produkt("telewizor",new BigDecimal("3500"));
+        Produkt p6=new Produkt("szafa",new BigDecimal("190.50"));
+        Produkt p7=new Produkt("obraz",new BigDecimal("3200.99"));
+        Produkt p8=new Produkt("komoda",new BigDecimal("500"));
+        
+        Koszyk k=new Koszyk();
+        k.dodaj(p2);
+        k.dodaj(p3);
+        k.dodaj(p2);
+        
+        System.out.println(k.doZaplaty());
+        
+        System.out.println(k.toString());
+        
     }
     
 }
